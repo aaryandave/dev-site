f920fe54bd44eaf5d14893f4ae46e4e75ce0764e
Aaryan Dave <aaryan.r.dave@gmail.com>
2024-12-16 02:10:15 -0500

    Updated site

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/aaryandave/",
+		label: "Linkedin",
+		handle: "aaryandave",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:aaryan.r.dave@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "aaryan.r.dave@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/aaryandave",
 		label: "Github",
-		handle: "chronark",
+		handle: "aaryandave",
 	},
 ];
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "aaryandave.dev",
+    template: "%s | aaryandave.dev",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "CS @ Purdue, SWE @ Marriott",
   openGraph: {
-    title: "chronark.com",
+    title: "aaryandave.dev",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "CS @ Purdue, SWE @ Marriott",
+    url: "https://aaryandave.dev",
+    siteName: "aaryandave.dev",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://aaryandave.dev/og.png",
         width: 1920,
         height: 1080,
       },
@@ -37,8 +37,8 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  twitter: {
-    title: "Chronark",
+  linkedin: {
+    title: "@aaryandave",
     card: "summary_large_image",
   },
   icons: {
